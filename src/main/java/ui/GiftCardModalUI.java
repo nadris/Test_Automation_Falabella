9c1fbf283da0138ab6accf27e80a7057c069734d
Noemi Ancari <noemiancari.na@gmail.com>
2022-09-27 18:28:25 -0400

    paths

--- src/main/java/ui/GiftCardModalUI.java
+++ src/main/java/ui/GiftCardModalUI.java
@@ -1,4 +1,8 @@
 package ui;
 
+import org.openqa.selenium.By;
+
 public class GiftCardModalUI {
+
+    public static By giftCardModal = By.className("fbra_defaultModal fbra_payment-options-redesign");
 }
